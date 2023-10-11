e0c447696c32b545780cb998eacd7bcc7fd1705b
vertigo235 <jbjonesclt@gmail.com>
2023-10-11 11:28:07 -0400

    update NUGS ENV names

--- app/nugsalert.sh
+++ app/nugsalert.sh
@@ -9,13 +9,13 @@ groupadd -g $APP_GROUP_ID appgroup
 useradd -u $APP_USER_ID -g $APP_GROUP_ID -m appuser
 
 # Set default values for variables
-EMAIL=${EMAIL:-"youremail@email.com"}
-PASSWORD=${PASSWORD:-"your_password"}
-FORMAT=${FORMAT:-4}
-VIDEO_FORMAT=${VIDEO_FORMAT:-5}
-OUT_PATH=${OUT_PATH:-"/downloads"}
-TOKEN=${TOKEN:-}
-USE_FFMPEG_ENV_VAR=${USE_FFMPEG_ENV_VAR:-true}
+EMAIL=${NUGS_EMAIL:-"youremail@email.com"}
+PASSWORD=${NUGS_PASSWORD:-"your_password"}
+FORMAT=${NUGS_FORMAT:-4}
+VIDEO_FORMAT=${NUGS_VIDEO_FORMAT:-5}
+OUT_PATH=${NUGS_OUT_PATH:-"/downloads"}
+TOKEN=${NUGS_TOKEN:-}
+USE_FFMPEG_ENV_VAR=${NUGS_USE_FFMPEG_ENV_VAR:-true}
 JSON_FILE="/app/config.json"
 
 # Check if the config file already exists
